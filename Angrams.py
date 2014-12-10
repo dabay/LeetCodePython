9a3b4a051973c53898e1f97651895d160e6003d6
dabay <dabay.wong@gmail.com>
2014-12-10 21:25:55 +0000

    Add file

---
+++ Angrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param strs, a list of strings
+    # @return a list of strings
+    def anagrams(self, strs):
+        pass
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.anagrams()
+    
