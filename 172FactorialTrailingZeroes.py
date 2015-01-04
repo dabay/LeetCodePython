533b9d49269c30662c890af1a1488e4279bad7ea
dabay <dabay.wong@gmail.com>
2015-01-04 14:26:56 +0000

    Accepted.

---
+++ 172FactorialTrailingZeroes.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+'''
+https://oj.leetcode.com/problems/factorial-trailing-zeroes/
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+'''
+
+class Solution:
+    # @return an integer
+    def trailingZeroes(self, n):
+        result = 0
+        temp = n
+        while temp >= 5:
+            temp = temp / 5
+            result = result + temp
+        return result
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 30
+    print s.trailingZeroes(n)
+    import math
+    #print math.factorial(n)
