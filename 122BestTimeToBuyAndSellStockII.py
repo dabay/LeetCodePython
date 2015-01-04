a9b790a6e5430fa23aaaa0ca6ea4916e7bb5c8f4
dabay <dabay.wong@gmail.com>
2015-01-04 21:13:04 +0000

    Accepted.

--- 122BestTimeToBuyAndSellStockII.py
+++ 122BestTimeToBuyAndSellStockII.py
@@ -7,6 +7,8 @@ Say you have an array for which the ith element is the price of a given stock on
 Design an algorithm to find the maximum profit. You may complete as many transactions as you like
 (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple
 transactions at the same time (ie, you must sell the stock before you buy again).
+
+这个更快一点，只是有点tricky.
 '''
 
 class Solution:
@@ -16,21 +18,9 @@ class Solution:
         if len(prices) <= 1:
             return 0
         profit = 0
-        buy_value = prices[0]
-        hold = False
         for i in xrange(1, len(prices)):
-            if prices[i] == prices[i-1]:
-                continue
-            if prices[i-1] < prices[i] and hold is False:
-                buy_value = prices[i-1]
-                hold = True
-                continue
-            if prices[i-1] > prices[i] and hold is True:
-                profit = profit + (prices[i-1] - buy_value)
-                hold = False
-                continue
-        if hold is True:
-            profit = profit + (prices[-1] - buy_value)
+            if prices[i-1] < prices[i]:
+                profit = profit + (prices[i] - prices[i-1])
         return profit
 
 
