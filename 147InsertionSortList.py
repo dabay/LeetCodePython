f5e56ff0c9559a75fc633cc4e7b92a8e127b503d
dabay <dabay.wong@gmail.com>
2015-01-08 17:51:20 +0000

    Accepted.

--- 147InsertionSortList.py
+++ 147InsertionSortList.py
@@ -15,55 +15,72 @@ class Solution:
     # @param head, a ListNode
     # @return a ListNode
     def insertionSortList(self, head):
-        def insert(head, node, end):
-            if node.val < head.val:
-                node.next = head
-                return (node, end)
-            n = head
-            while n.next.val < node.val and n != end:
-                n = n.next
-
-            if n == end:
-                node.next = end.next
-                n.next = node
-                return (head, node)
-            else:
-                end.next = node.next
-                node.next = n.next
-                n.next = node
-                return (head, end)
-
         if head is None or head.next is None:
             return head
-        if head.next.next is None:
-            if head.val <= head.next.val:
-                return head
-            else:
-                new_head = head.next
-                new_head.next = head
-                head.next = None
-                return new_head
-
-        new_head = head
-        new_end = head
+        new_head = ListNode(-1)
+        new_head.next = head
         cursor = head.next
-        new_end.next = cursor.next
+        head.next = None
         while cursor:
-            (new_head, new_end) = insert(new_head, cursor, new_end)
-            cursor = new_end.next
+            next = cursor.next
+            previous = new_head
+            while previous.next is not None and previous.next.val < cursor.val:
+                previous = previous.next
+            cursor.next = previous.next
+            previous.next = cursor
+            cursor = next
+        return new_head.next
 
-        return new_head
+
+        # def insert(head, node, end):
+        #     if node.val < head.val:
+        #         node.next = head
+        #         return (node, end)
+        #     n = head
+        #     while n.next.val < node.val and n != end:
+        #         n = n.next
+        #
+        #     if n == end:
+        #         node.next = end.next
+        #         n.next = node
+        #         return (head, node)
+        #     else:
+        #         end.next = node.next
+        #         node.next = n.next
+        #         n.next = node
+        #         return (head, end)
+        #
+        # if head is None or head.next is None:
+        #     return head
+        # if head.next.next is None:
+        #     if head.val <= head.next.val:
+        #         return head
+        #     else:
+        #         new_head = head.next
+        #         new_head.next = head
+        #         head.next = None
+        #         return new_head
+        #
+        # new_head = head
+        # new_end = head
+        # cursor = head.next
+        # new_end.next = cursor.next
+        # while cursor:
+        #     (new_head, new_end) = insert(new_head, cursor, new_end)
+        #     cursor = new_end.next
+        #
+        # return new_head
 
 
 def main():
     s = Solution()
-    n1 = ListNode(1)
-    n2 = ListNode(1)
-    # n3 = ListNode(3)
-    # n4 = ListNode(2)
+    n1 = ListNode(3)
+    n2 = ListNode(4)
+    n3 = ListNode(1)
+    n4 = ListNode(2)
     n1.next = n2
-    # n2.next = n3
-    # n3.next = n4
+    n2.next = n3
+    n3.next = n4
 
     n = n1
     while n:
@@ -71,9 +88,9 @@ def main():
         n = n.next
     print
 
-    s.insertionSortList(n1)
+    head = s.insertionSortList(n1)
 
-    n = n1
+    n = head
     while n:
         print n.val, "->",
         n = n.next
