8006360d4b0171c32831cdae901002559b42bd29
dabay <dabay.wong@gmail.com>
2015-01-11 16:39:48 +0000

    Accepted.

--- 2AddTwoNumbers.py
+++ 2AddTwoNumbers.py
@@ -9,6 +9,10 @@ Add the two numbers and return it as a linked list.
 
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
+
+===Comments by Dabay===
+这道题应该是要求不使用额外的空间。思路比较简单，就是逐个加起来的同时考虑进位。
+那就在已有的空间上操作，主要是处理一些细节问题，如末尾有进位、两个数组不一样长等等。
 '''
 
 # Definition for singly-linked list.
@@ -25,65 +29,45 @@ class Solution:
         if l2 is None:
             return l1
 
-        ret = l1
-        c1 = l1
-        c2 = l2
-        carry = 0
-
-        v = c1.val + c2.val + carry
-
-        if v >= 10:
-            v = v - 10
-            carry = 1
-        else:
-            carry = 0
-        c1.val = v
-
-        while c1.next is not None and c2.next is not None:
-            c1 = c1.next
-            c2 = c2.next
-            v = c1.val + c2.val + carry
+        node1, node2, carry, end = l1, l2, 0, None
+        while node1 or node2:
+            if not node1:
+                node1 = ListNode(0)
+                end.next = node1
+            if not node2:
+                node2 = ListNode(0)
+            v = node1.val + node2.val + carry
             carry = 0
             if v >= 10:
                 v = v - 10
                 carry = 1
-            else:
-                carry = 0
-            c1.val = v
+            node1.val = v
+            end = node1
+            node1 = node1.next
+            node2 = node2.next
+        if carry:
+            end.next = ListNode(1)
+        return l1
 
-        if c1.next is None and c2.next is None:
-            if carry == 1:
-                c1.next = ListNode(1)
-                return ret
-            return ret
 
-        if c1.next is None and c2.next is not None:
-            tail = c2.next
-            while c2.next is not None:
-                c2 = c2.next
-                v = c2.val + carry
+def main():
+    root1 = ListNode(2)
+    root1.next = ListNode(4)
+    root1.next.next = ListNode(3)
+    root2 = ListNode(5)
+    root2.next = ListNode(6)
+    root2.next.next = ListNode(4)
+    s = Solution()
+    root = s.addTwoNumbers(root1, root2)
+    node = root
+    while node:
+        print "%s->" % node.val,
+        node = node.next
+    print "None"
 
-                if v >= 10:
-                    v = v -10
-                    carry = 1
-                else:
-                    carry = 0
-                c2.val = v
-            if carry == 1:
-                c2.next = ListNode(1)
-            c1.next = tail
-            return ret
 
-        if c1.next is not None and c2.next is None:
-            while c1.next is not None:
-                c1 = c1.next
-                v = c1.val + carry
-                if v >= 10:
-                    v = v -10
-                    carry = 1
-                else:
-                    carry = 0
-                c1.val = v
-            if carry == 1:
-                c1.next = ListNode(1)
-            return ret
+if __name__ == "__main__":
+    import time
+    start = time.clock()
+    main()
+    print "%s sec" % (time.clock() - start)
