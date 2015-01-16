c35096bf38a7f2788a8b5d32550d18137a555e3c
dabay <dabay.wong@gmail.com>
2015-01-16 21:56:39 +0000

    Accepted. DP.

--- 10RegularExpressionMatching.py
+++ 10RegularExpressionMatching.py
@@ -22,89 +22,59 @@ isMatch("ab", ".*") → true
 isMatch("aab", "c*a*b") → true
 
 ===Comments by Dabay===
-自我感觉很解得很垃圾啊。如果不是把p做了压缩还过不了online judge。
+这道题应该用动态规划的做法。我只想说，头大！
 
-当s和p都是空的时候，匹配成功。
-如果s为空，p不为空，检查p的偶数为是不是都是*。
+建立一个二维数组，第一维是s为基础，第二维是p为基础。（顺序很重要）
+初始化res[0]，长度比p多1，即多第一个true。同时考虑p中开始的几个偶数为是*的情况，初始化相应的true。其他为false。
 
-如果s和p都不为空，头一个字母
-    如果不匹配，
-        不带*，False
-        带*，递归p[2:]
-    如果匹配，
-        不带*，递归s[1:],p[1:]
-        带*，考虑匹配0到最远端的情况，分别递归s[i:],p[2:]
+然后每次放一个s中元素进来，尝试匹配整个p。
+
+三种情况：（每次更新的是res[i+1][j+1]）
+    如果p[j]不是.和*，即是一般比较，同时观察res[i][j]。
+    如果p[j]是.，无需比较，根据res[i][j]的值即可。
+    如果p[j]是*，
+        考虑匹配零次的情况：判断res[i+1][j-1]是否为true
+        考虑匹配一次的情况：判断res[i+1][j]是否为true
+        考虑匹配多次的情况：判断字符是否相符，同时判断res[i][j+1]
 '''
 class Solution:
     # @return a boolean
     def isMatch(self, s, p):
-        def compress(p):
-            i = 0
-            while i < len(p)-3:
-                if p[i+1] != '*':
-                    i = i + 1
-                    continue
-                if p[i+3] == '*':
-                    if p[i] == "." or p[i+2] == ".":
-                        p = p[:i] + ".*" + p[i+4:]
-                        continue
-                    elif p[i] == p[i+2]:
-                        p = p[:i] + p[i+2:]
-                        continue
-                i = i + 2
-            return p
+        res = []
+        defaultRow = [False] + [False for _ in p]
+        res.append(list(defaultRow))
+        res[0][0] = True
+        for x in xrange(len(p)):
+            if p[x] == '*':
+                res[0][x+1] = res[0][x-1]
 
-        def isMatch2(s, p):
-            if len(s) == 0:
-                if len(p) == 0:
-                    return True
-                if len(p) % 2 == 0:
-                    i = 1
-                    while i < len(p):
-                        if p[i] != "*":
-                            return False
-                        i = i + 2
-                    else:
-                        return True
-                else:
-                    return False
-            if len(s) > 0 and len(p) == 0:
-                return False
-
-            match_char = p[0]
-            multi = False
-            if len(p) > 1:
-                if p[1] == "*":
-                    multi = True
-
-            if match_char != s[0] and match_char != ".":
-                if multi:
-                    return isMatch2(s, p[2:])
-                else:
-                    return False
-
-            if multi is False:
-                return isMatch2(s[1:], p[1:])
-            else:
-                result = False
-                i = 0
-                result = isMatch2(s, p[2:])
-                while i < len(s):
-                    if result == True:
-                        return result
-                    if (s[i] == match_char or match_char == "."):
-                        result = isMatch2(s[i+1:], p[2:])
-                    else:
-                        break
-                    i = i + 1
-                return result
-
-        return isMatch2(s, compress(p))
+        for i in xrange(len(s)):
+            res.append(list(defaultRow))
+            for j in xrange(len(p)):
+                if p[j] != '*':
+                    if (s[i] == p[j] or p[j] == '.') and res[i][j]:
+                        res[i+1][j+1] = True
+                elif p[j] == '.':
+                    if res[i][j]:
+                        res[i+1][j+1] = True
+                else: # *
+                    #匹配0次
+                    if res[i+1][j-1]:
+                        res[i+1][j+1] = True
+                    #匹配1次
+                    if res[i+1][j]:
+                        res[i+1][j+1] = True
+                    #匹配多次
+                    if (s[i] == p[j-1] or p[j-1] == '.') and res[i][j+1]:
+                        res[i+1][j+1] = True
+        return res[-1][-1]
 
 
 def main():
-    s = Solution()
-    print s.isMatch("aa", "a*")
+    sol = Solution()
+    s = "aab"
+    p = "c*a*b"
+    print sol.isMatch(s, p)
 
 
 if __name__ == "__main__":
