62b03230ba845b84e434d379d840d7af287bd334
dabay <dabay.wong@gmail.com>
2015-01-29 14:14:11 +0000

    Accepted.

--- 25ReverseNodesInKGroup.py
+++ 25ReverseNodesInKGroup.py
@@ -1,33 +1,26 @@
 # -*- coding: utf8 -*-
 '''
 __author__ = 'dabay.wang@gmail.com'
+
+25: Reverse Nodes in k-Group
 https://oj.leetcode.com/problems/reverse-nodes-in-k-group/
 
 Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
-
 If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
-
 You may not alter the values in the nodes, only nodes itself may be changed.
-
 Only constant memory is allowed.
 
 For example,
 Given this linked list: 1->2->3->4->5
-
 For k = 2, you should return: 2->1->4->3->5
-
 For k = 3, you should return: 3->2->1->4->5
 
 ===Comments by Dabay===
 前面加一个辅助head。
-每次处理k个元素，如果最后剩下的元素不足k，直接加到末尾。
-
-reverse处理k个元素的时候，需要两个指针：指向第一个元素的前一个指针，以及 最后一个元素的后一个指针。
-记得返回新的reverse之后的最后一个指针作为，下一次reverse的第一个参数。
-
-reverse的时候用三个指针分别代表原链表中的pre,cur,next。
-
-因为每次都先找到需要k个元素的区间，然后在调整指针，相当于遍历两次了，应该再改进一些...
+用一个栈来记录k个元素，当栈不满的时候入栈，当栈大小到k的时候处理这k个元素。
+最后检查栈是否为空
+    如果为空，末尾加None
+    如果不为空，末尾指向栈底的元素即可（因为入栈的时候，并没有破坏链表顺序）
 '''
 
 # Definition for singly-linked list.
@@ -42,28 +35,22 @@ class Solution:
     # @param k, an integer
     # @return a ListNode
     def reverseKGroup(self, head, k):
-        def reverse(previous, after):
-            new_previous = previous.next
-            pre = previous.next
-            cur = pre.next
-            while cur != after:
-                next = cur.next
-                cur.next = pre
-                pre = cur
-                cur = next
-            previous.next = pre
-            new_previous.next = after
-            return new_previous
-
         previous = new_head = ListNode(0)
-        new_head.next = n = head
-        counter = 0
-        while n:
-            n = n.next
-            counter = counter + 1
-            if counter == k:
-                previous = reverse(previous, n)
-                counter = 0
+        node = new_head.next = head
+        stack = []
+        while node:
+            stack.append(node)
+            node = node.next
+            if len(stack) == k:
+                while len(stack) > 0:
+                    pop_node = stack.pop()
+                    previous.next = pop_node
+                    previous = pop_node
+        else:
+            if len(stack) > 0:
+                previous.next = stack[0]
+            else:
+                previous.next = None
         return new_head.next
 
 
@@ -77,11 +64,11 @@ def print_listnode(node):
 def main():
     sol = Solution()
     node = root = ListNode(1)
-    for i in xrange(2, 15):
+    for i in xrange(2, 3):
         node.next = ListNode(i)
         node = node.next
     print_listnode(root)
-    print_listnode(sol.reverseKGroup(root, 1))
+    print_listnode(sol.reverseKGroup(root, 2))
 
 
 if __name__ == "__main__":
