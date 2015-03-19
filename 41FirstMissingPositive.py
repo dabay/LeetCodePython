a9587f72ffcd49731217cb681d14dc59e9b47f13
dabay <dabay.wong@gmail.com>
2015-03-19 14:20:04 +0000

    Accepted.

--- 41FirstMissingPositive.py
+++ 41FirstMissingPositive.py
@@ -1,16 +1,16 @@
 # -*- coding: utf8 -*-
 '''
 __author__ = 'dabay.wang@gmail.com'
+
+41: First Missing Positive
 https://oj.leetcode.com/problems/first-missing-positive/
-First Missing Positive
 
 Given an unsorted integer array, find the first missing positive integer.
-
 For example,
 Given [1,2,0] return 3,
 and [3,4,-1,1] return 2.
-
 Your algorithm should run in O(n) time and uses constant space.
+
 ===Comments by Dabay===
 要求O(n)的时间，肯定不能常规排序，意思是只能扫描一次。
 因为对空间有要求，所以只能在已经有的空间上做文章。
@@ -19,7 +19,7 @@ Your algorithm should run in O(n) time and uses constant space.
 就把它交换到下标和它一样的位置上。
 这样，扫描完成之后，从1的位置开始判断，如果位置k上存的数字不是k，这就是缺少的第一个正数。
 
-这套题有三个需要注意的地方：
+这道题有三个需要注意的地方：
     -   交换之后，下标不能移动，需要继续判断。
     -   可能从1开始到最后都没有缺少的正数，此时下一个正数可能放在第一个位置上。
     -   当数组长度为0时，直接返回1.
