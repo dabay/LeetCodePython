0ac2c1a0ce0233035733e53d378e04aa1771edb5
dabay <dabay.wong@gmail.com>
2015-03-19 15:43:59 +0000

    Accepted.

--- 42TrappingRainWater.py
+++ 42TrappingRainWater.py
@@ -1,76 +1,48 @@
 # -*- coding: utf8 -*-
 '''
-Trapping Rain Water
+__author__ = 'dabay.wang@gmail.com'
+
+42: Trapping Rain Water
 https://oj.leetcode.com/problems/trapping-rain-water/
 
 Given n non-negative integers representing an elevation map where the width of each bar is 1,
 compute how much water it is able to trap after raining.
-
 For example,
 Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 
 ===Comments by Dabay===
-臃肿的解法，没什么好说的。等下次优化之后再写评论。
+请参考：http://blog.csdn.net/wzy_1988/article/details/17752809
+挨个分析每个A[i]能trapped water的容量，然后将所有的A[i]的trapped water容量相加即可
+其次，对于每个A[i]能trapped water的容量，取决于A[i]左右两边的高度（可延展）较小值与A[i]的差值，
+即volume[i] = [min(left[i], right[i]) - A[i]] * 1，这里的1是宽度，如果the width of each bar is 2,那就要乘以2了
 '''
 
 class Solution:
     # @param A, a list of integers
     # @return an integer
     def trap(self, A):
-        def find_right_index(left_pole_index, A):
-            right_pole_index = left_pole_index + 1
-            while right_pole_index < len(A):
-                if A[right_pole_index] >= A[left_pole_index]:
-                    return  right_pole_index
-                right_pole_index = right_pole_index + 1
-            else:
-                return right_pole_index
-
-        def trap_water(left_pole_index, right_pole_index, A):
-            water = 0
-            left_pole = A[left_pole_index]
-            index = left_pole_index + 1
-            while index < right_pole_index:
-                water = water + (left_pole - A[index])
-                index = index + 1
-            return water
-
-        def find_max_index(A):
-            max_index = 0
-            for i in xrange(len(A)):
-                if A[max_index] < A[i]:
-                    max_index = i
-            return max_index
-
-        def trap2(part):
-            if part is None or len(part) <= 2:
-                return 0
-            water = 0
-            left_pole_index = 0
-            while left_pole_index < len(part)-2:
-                right_pole_index = find_right_index(left_pole_index, part)
-                if right_pole_index < len(part):
-                    water = water + trap_water(left_pole_index, right_pole_index, part)
-                    left_pole_index = right_pole_index
-                else:
-                    left_pole_index = left_pole_index + 1
-            return water
-
         if len(A) <= 2:
             return 0
 
-        max_index = find_max_index(A)
-        left_part = A[:max_index+1]
-        right_part = A[max_index:]
-        right_part.reverse()
+        highest_on_left = [A[0] for _ in A]
+        for i in xrange(1, len(A)):
+            highest_on_left[i] = max(highest_on_left[i-1], A[i])
 
-        return trap2(left_part) + trap2(right_part)
+        highest_on_right = [A[-1] for _ in A]
+        for i in xrange(len(A)-2, -1, -1):
+            highest_on_right[i] = max(highest_on_right[i+1], A[i])
+
+        res = 0
+        for i in xrange(1, len(A)-1):
+            res += min(highest_on_left[i], highest_on_right[i]) - A[i]
+        return res
 
 
 def main():
     s = Solution()
-    nums = [4,2,3]
+    nums = [0,1,0,2,1,0,1,3,2,1,2,1]
     print s.trap(nums)
+
 
 if __name__ == "__main__":
     import time
