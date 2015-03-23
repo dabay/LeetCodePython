475acb1380ace6d6c41f818c271b47a1a0e9000d
dabay <dabay.wong@gmail.com>
2015-03-23 14:31:13 +0000

    Accepted.

--- 44WildcardMatching.py
+++ 44WildcardMatching.py
@@ -1,17 +1,15 @@
 # -*- coding: utf8 -*-
 '''
 __author__ = 'dabay.wang@gmail.com'
-https://oj.leetcode.com/problems/wildcard-matching/
+
 44：Wildcard Matching
+https://oj.leetcode.com/problems/wildcard-matching/
 
 '?' Matches any single character.
 '*' Matches any sequence of characters (including the empty sequence).
-
 The matching should cover the entire input string (not partial).
-
 The function prototype should be:
 bool isMatch(const char *s, const char *p)
-
 Some examples:
 isMatch("aa","a") → false
 isMatch("aa","aa") → true
@@ -20,10 +18,11 @@ isMatch("aa", "*") → true
 isMatch("aa", "a*") → true
 isMatch("ab", "?*") → true
 isMatch("aab", "c*a*b") → false
+
 ===Comments by Dabay===
 使用动态规划的做法.
-先想象在s和p的前面都加一个空格，以s为外循环，p为循环。
-先初始化第一组数据，第一个“空”匹配“空”，所以第一个为True。然后看如果后面是*，就继续设置为True。
+先想象在s和p的前面都加一个空格，以s为外循环，p为内循环。
+先初始化第一组数据，第一个“空”匹配“空”，所以第一个为True。然后看如果后面如果是*就继续设置为True。
 
 进入循环之后，判断根据p中的字符
     - 如果是？*以外的字符，就判断这个字符和s中对应的字符是否一致，而且s中到上一个字符和p中到上一次字符匹配，设True
