eb49e641f1416dd8ef60cbf152b0620ffb95253d
dabay <dabay.wong@gmail.com>
2015-03-24 17:36:07 +0000

    Accepted.

--- 47PermutationsII.py
+++ 47PermutationsII.py
@@ -12,37 +12,36 @@ For example,
 [1,1,2], [1,2,1], and [2,1,1].
 
 ===Comments by Dabay===
-生成排列。
-既然有重复的，就维护一个hash来记录那些已经处理过的情况。没想到就过了OJ。但是用时比较多
-应该有精妙的解法，等下次google一下。
+先排序，然后DFS。
+注意去重，如果进入了一次DFS，用pre记录上次用的数字。这个pre只在DFS中的for中有效，所以每次进入进入新的DFS的时候，重新设置为None.
 '''
 
 class Solution:
     # @param num, a list of integer
     # @return a list of lists of integers
     def permuteUnique(self, num):
-        def permutation2(result, checked, per, rest):
-            if len(rest) == 0:
-                result.append(list(per))
-                return
-            for i in xrange(len(rest)):
-                per.append(rest[i])
-                r = rest[:i] + rest[i+1:]
-                if str(per) not in checked:
-                    checked[str(per)] = True
-                    permutation2(result, checked, per, r)
-                per.pop(-1)
+        res = []
+        num.sort()
+        self.DFS(res, [], None, num)
+        return res
 
-        result = []
-        checked = {}
-        permutation2(result, checked, [], num)
-        return result
+    def DFS(self, res, l, pre, nums):
+        if len(nums) == 0:
+            res.append(list(l))
+        pre = None
+        for i in xrange(len(nums)):
+            if nums[i] == pre:
+                continue
+            l.append(nums[i])
+            self.DFS(res, l, nums[i], nums[:i] + nums[i+1:])
+            l.pop()
+            pre = nums[i]
 
 
 def main():
     sol = Solution()
-    nums = [1,1,1,1,2,3]
-    print len(sol.permuteUnique(nums))
+    nums = [1,1,2]
+    print sol.permuteUnique(nums)
 
 
 if __name__ == "__main__":
